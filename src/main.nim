4ddab6c19a1ad68181b9917e1b010cd9e3455a7d
purofle <3272912942@qq.com>
2021-08-29 21:59:31 +0800

    add main.nim

---
+++ src/main.nim
@@ -0,0 +1,13 @@
+import strutils
+import std/os
+
+let file = open("./grub.cfg", fmReadWriteExisting)
+var content = file.readAll()
+content = replace(content, "### END /etc/grub.d/00_header ###", """### END /etc/grub.d/00_header ###
+menuentry fuckGRUB {
+  echo FUCK YOU!
+  play 1750 523 1 392 1 523 1 659 1 784 1 1047 1 784 1 415 1 523 1 622 1 831 1 622 1 831 1 1046 1 1244 1 1661 1 1244 1 466 1 587 1 698 1 932 1 1195 1 1397 1 1865 1 1397 1
+  sleep 3
+}""")
+file.write(content)
+file.close()
