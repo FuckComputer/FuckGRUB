d50d23d7271bc0a361618c10e99f811a1d53e5d4
purofle <3272912942@qq.com>
2021-08-30 15:13:29 +0800

    fix bug

--- src/main.nim
+++ src/main.nim
@@ -5,14 +5,16 @@ let input = readLine(stdin)
 var path = "/boot/grub/grub.cfg"
 if input != "":
   path = input
-let file = open(path, fmReadWriteExisting)
+var file = open(path)
 var content = file.readAll()
+file.close()
 content = replace(content, "### END /etc/grub.d/00_header ###", """### END /etc/grub.d/00_header ###
 menuentry fuckGRUB {
   echo FUCK YOU!
   play 1750 523 1 392 1 523 1 659 1 784 1 1047 1 784 1 415 1 523 1 622 1 831 1 622 1 831 1 1046 1 1244 1 1661 1 1244 1 466 1 587 1 698 1 932 1 1195 1 1397 1 1865 1 1397 1
   sleep 3
 }""")
+file = open(path, fmWrite)
 file.write(content)
 file.close()
 echo "FINISH!"
