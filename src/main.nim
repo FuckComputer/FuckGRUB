391e2ab6528094b8496fe587dc3baa1e8e4f58a7
purofle <3272912942@qq.com>
2021-08-30 15:46:04 +0800

    mopemope

--- src/main.nim
+++ src/main.nim
@@ -9,7 +9,7 @@ var file = open(path)
 var content = file.readAll()
 file.close()
 content = replace(content, "### END /etc/grub.d/00_header ###", """### END /etc/grub.d/00_header ###
-menuentry mope mope {
+menuentry 'mope mope' {
   echo ~m~o~p~e~m~o~p~e/Leaf
   play 300 784 2 659 2 532 2 784 2 784 1 880 1 784 1 699 1 784 2 784 1 1047 1 932 2 932 1 880 1 784 2 699 2 659 2 699 2 784 4 784 2 659 2 532 2 784 2 784 1 880 1 784 1 699 1 784 2 880 1 932 1 1047 2 699 1 659 1 699 2 1047 2 932 2 880 2 784 2
   sleep 3
